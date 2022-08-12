f0fb6c61851f3cbfdeb15b4a5d2bf3aaff5cf958
Adrian Gonzalez <adriangonzalez1501@gmail.com>
2022-08-11 22:48:09 -0600

    Solved Exercise 6

--- EjerciciosPracticos9/Ejercicio6.py
+++ EjerciciosPracticos9/Ejercicio6.py
@@ -1,11 +1,8 @@
 def main():
-    mx = [[1,12,13,40],
-          [50,66,72,80],
-          [9,10,22,33],
-          [4,5,6,7]]
+    mx = [[10,11,14,16],[20,18,16,14],[18,20,23,24],[12,10,8,6]]
     printMatrix(mx)
-    rotMx = rotateMatrix(mx)
-    printMatrix(rotMx)
+    mx = rotateMatrix(mx)
+    printMatrix(mx)
 
 def printMatrix(mx):
     for line in mx:
@@ -13,13 +10,18 @@ def printMatrix(mx):
     print("\n")
 
 def rotateMatrix(mx):
-    rotMx = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+    tempMx = []
+    col = len(mx) -1
 
-    for col in range(0,len(mx)):
+    for col in range(len(mx)-1,-1,-1):
+        temp = []
+        for line in range(len(mx)):
+            temp.append(mx[line][col])
+        tempMx.append(temp)
 
-        for line in range(0,len(mx)):
-            rotMx[line][col] = mx[line][col]
-            
-    return rotMx
+    for x in range(len(mx)):
+        for y in range(len(mx)):
+            mx[x][y] = tempMx[x][y]
+    return mx
 
 main()
